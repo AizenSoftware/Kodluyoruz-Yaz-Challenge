d54b35726ac99c07fb2e07c5475c6fc25d1e51f6
Caner Büyükduru <47858808+AizenSoftware@users.noreply.github.com>
2023-09-08 19:29:39 +0300

    Hard Skill Challenge 5

---
+++ Hafta-9/script.js
@@ -0,0 +1,6 @@
+import easy from "./scripts/easy.js"; 
+
+// Easy
+  const sayi1 = prompt("Lütfen bir sayi giriniz");
+  const sayi2 = prompt("Lütfen ikinci sayiyi giriniz");
+  easy(parseInt(sayi1),parseInt(sayi2));
---
+++ Hafta-9/scripts/easy.js
@@ -0,0 +1,4 @@
+export default easy;
+ function easy(sayi1, sayi2){
+   return console.log(sayi1 + sayi2);
+ }
