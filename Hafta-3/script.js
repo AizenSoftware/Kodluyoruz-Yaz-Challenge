bb1b51d13654e443052fbd6bfcc52bea1d69830e
Caner Büyükduru <47858808+AizenSoftware@users.noreply.github.com>
2023-09-08 19:00:43 +0300

    Hard Skill Challenge 12

---
+++ Hafta-3/script.js
@@ -0,0 +1,6 @@
+import easy from "./scripts/easy.js"; 
+import medium from "./scripts/medium.js";
+import hard from "./scripts/hard.js";
+
+// Easy
+  easy();
---
+++ Hafta-3/scripts/medium.js
@@ -0,0 +1,5 @@
+export default upperCaseKelime;
+function upperCaseKelime(string){
+    console.log(`Orijinal Kelime: ${string}`);
+    console.log(`Büyük Harfli Kelime: ${string.toUpperCase()}`);
+}
