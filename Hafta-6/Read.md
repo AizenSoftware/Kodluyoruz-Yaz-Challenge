cf017ab37f933283355aae0cf4ca5f113cfdb3d2
Caner Büyükduru <47858808+AizenSoftware@users.noreply.github.com>
2023-09-08 19:13:46 +0300

    Hard Skill Challenge 9

---
+++ Hafta-6/Read.md
@@ -0,0 +1,3 @@
+Challenge 9 Hard Challenge 
+
+🙌🏼 Easy: Kullanıcıdan aldığınız sayının tek mi çift mi olduğunu ekrana yazdıran bir kod parçacığı yazar mısın?
---
+++ Hafta-6/scripts/easy.js
@@ -0,0 +1,8 @@
+export default easy;
+ function easy(sayi){
+   if(sayi%2 === 0){
+    console.log("Sayı Çifttir.");
+   }else{
+    console.log("Sayi tektir");
+   }
+ }
